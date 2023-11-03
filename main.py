4c6dd155ddee1916cd0f99e269e4d1f6efc53a1f
junior1109 <ttk11092k3@gmail.com>
2023-11-04 00:16:23 +0700

    start

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ app.static_folder = "static"
 def index():
     return render_template('index.html', header_nike = "")
 
+@app.route('/searchData', methods=['GET', 'POST'])
+def searchData(search_text=""):
+    sqldbname = 'LapTrinhWeb/python/db/website.db'
+    if search_text != "":
+        conn=sqlite3.connect(sqldbname)
+        cursor = conn.cursor()
+        sqlcommand = ("Select * from storages "
+                      "where model like '%" +search_text+"%'")
+        sqlcommand = sqlcommand + " or brand like '%"+search_text+ "%'"
+        sqlcommand = sqlcommand + " or details like '%"+search_text+ "%'"
+        
+        cursor.execute(sqlcommand)
+        data = cursor.fetchall()
+        conn.close()
+        return data
 if __name__ == '__main__':
     app.debug = True
     app.run()
