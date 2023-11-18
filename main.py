fd2239b83c165e4c3d17e90bc12801db71a1414b
junior1109 <ttk11092k3@gmail.com>
2023-11-18 10:38:38 +0700

    18/11/2023

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# <!-- Đây là trang chính
+#     Viết code nhớ comment
+#     Anh em thêm/sửa/xóa code thì nhớ comment lại 
+#     Và push code lên git
+# -->
+# <!-- 
+#     git add .
+#     git commit -m "comment"
+#     git push origin master
+#  -->
+
 from flask import Flask, request, render_template, session, url_for, redirect
 import sqlite3
 
@@ -6,17 +17,19 @@ app.secret_key = 'your_secret_key'
 app.template_folder = "templates"
 app.static_folder = "static"
 
+# Hien trang chu len
 @app.route('/')
 def index():
-    return render_template('index.html', header_nike = "")
-
-@app.route('/searchData', methods=['GET', 'POST'])
-def searchData(search_text=""):
-    sqldbname = 'LapTrinhWeb/python/db/website.db'
+    return render_template("index.html.jinja2", search_text = "")
+    
+#tim kiem san pham trong database
+@app.route("/search", methods=['GET', 'POST'])
+def load_data_from_db(search_text=""):
+    sqldbname = 'db/data.db'
     if search_text != "":
         conn=sqlite3.connect(sqldbname)
         cursor = conn.cursor()
-        sqlcommand = ("Select * from storages "
+        sqlcommand = ("Select * from nike "
                       "where model like '%" +search_text+"%'")
         sqlcommand = sqlcommand + " or brand like '%"+search_text+ "%'"
         sqlcommand = sqlcommand + " or details like '%"+search_text+ "%'"
@@ -25,7 +38,31 @@ def searchData(search_text=""):
         data = cursor.fetchall()
         conn.close()
         return data
+
+#trang login
+@app.route('/login', methods=['GET', 'POST'])
+def check_exists(username, password):
+    result = False;
+    sqldbname = 'db/data.db'
+    conn = sqlite3.connect(sqldbname)
+    cursor = conn.cursor()
+    sqlcommand = "Select * from user where name = '"+username+"' and password = '"+password+"'"
+    cursor.execute(sqlcommand)
+    data = cursor.fetchall()
+    print(type(data))
+    if len(data)>0:
+        result = True
+    conn.close()
+    return result;
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if check_exists(username, password):
+            session['username'] = username 
+        return redirect(url_for('index'))
+    return render_template('login.html.jinja2')
+
+
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
-    
+    app.run(debug=True)
